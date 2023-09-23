fe98372bb5b672e331c7df65bd074bcf018e31f7
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-23 23:25:42 +0600

    codes added

--- src/socket/messageshare/client/Client1.java
+++ src/socket/messageshare/client/Client1.java
@@ -1,2 +1,44 @@
-package socket.messageshare.client;public class Client1 {
+package socket.messageshare.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client1 {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 5000);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            // Start a thread to handle server messages
+            Thread serverListener = new Thread(() -> {
+                try {
+                    while (true) {
+                        String message = dataInputStream.readUTF();
+                        System.out.println("[SERVER] " + message);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            });
+            serverListener.start();
+
+            while (true) {
+                //System.out.print("input> ");
+                String message = "client1: " + scanner.nextLine();
+                dataOutputStream.writeUTF(message);
+                if (message.equalsIgnoreCase("_stop"))
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
+
--- src/socket/messageshare/server/Server.java
+++ src/socket/messageshare/server/Server.java
@@ -1,2 +1,78 @@
-package socket.messageshare.server;public class Server {
+package socket.messageshare.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    public static List<ClientHandler> clients = new ArrayList<>(); // this will store
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Listening to port 5000");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(clientSocket + " connected\n");
+
+                // Create a new thread to handle this client
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
+                clientHandler.start();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
+
+class ClientHandler extends Thread {
+    private Socket clientSocket;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            dataInputStream = new DataInputStream(clientSocket.getInputStream());
+            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            String message;
+            while (true) {
+                message = dataInputStream.readUTF();
+                System.out.println(message);
+
+                // Broadcast the message to all connected clients
+                broadcast(message);
+
+                if (message.equalsIgnoreCase("_stop"))
+                    break;
+            }
+
+            clientSocket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void broadcast(String message) {
+        synchronized (Server.clients) {
+            for (ClientHandler client : Server.clients) {
+                try {
+                    client.dataOutputStream.writeUTF(message);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+
